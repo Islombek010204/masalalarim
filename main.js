e5e5e2f3a92d89b1d7a814e24f2feb3bddd2cbc2
Islombek010204 <tundraislom@gmail.com>
2024-06-12 22:26:02 +0500

    masalalar

---
+++ main.js
@@ -0,0 +1,116 @@
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i * i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i * i * i);
+// }
+
+// let number = [];
+// for (let i = 0; i < 10; i++) {
+// 	console.log(number.push(i));
+// }
+
+// let names = ['islombek', 'said', 'doniyor', 'mamur'];
+// for (let i = 0; i < names.length; i++) {
+// 	console.log(names[i]);
+// }
+
+// let number = [10, 10, 45];
+// let sum = 0;
+// for (let i = 0; i < number.length; i++) {
+// 	sum += number[i];
+// }
+// console.log(sum);
+
+// let soz = ['olma', 'anor', 'gilos'];
+// for (let i = 0; i < soz.length; i++) {
+// 	console.log(soz[i].toUpperCase());
+// }
+
+// let number = [5, 6];
+// let newArray = [];
+// for (let i = 0; i < number.length; i++) {
+// 	newArray.push(number[i] * 2);
+// }
+// console.log(newArray);
+
+// for (let i = 10; i > 0; i--) {
+// 	console.log(i);
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+// 	console.log(i);
+// }
+
+// for (let i = 1; i < 20; i += 2) {
+// 	console.log(i);
+// }
+
+// let sum = 0;
+// for (let i = 0; i < 10; i++) {
+// 	sum += i;
+// }
+// console.log(sum);
+
+// let yigindi = 0;
+// for (let i = 1; i <= 20; i++) {
+// 	yigindi += Math.pow(i, 3);
+// }
+// console.log(yigindi);
+
+// let yigindi = 0;
+// for (let i = 1; i <= 20; i++) {
+// 	yigindi += Math.pow(i, 3);
+// }
+// console.log(yigindi);
+
+// let kvadratlar = [];
+// for (let i = 1; i <= 20; i++) {
+// 	kvadratlar.push(i * i);
+// }
+// console.log(kvadratlar);
+
+// for (let i = 100; i >= 0; i -= 10) {
+// 	console.log(i);
+// }
+
+// let names = [
+// 	'java',
+// 	'paytn',
+// 	'javascript',
+// 	'dbobaujfeofbaufb aeubeu0apufbfuabfeufbaoufbeauo',
+// 	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem dolore consequatur fugiat sint quia facere magni quo eius laboriosam, architecto consequuntur dignissimos velit tempora fugit illo, veniam, veritatis provident suscipit? A commodi enim possimus aliquam. Dolorem earum qui tempora? Cumque eum minima, dolorem vero molestiae necessitatibus numquam quisquam animi saepe dolor, eaque veritatis laborum repudiandae possimus enim! Doloremque, quae fugiat? Quasi suscipit eius culpa voluptas, amet sed dolor optio, ipsa sit delectus illum! Beatae ex quidem sunt dolore quos eos tempore iure vero.',
+// ];
+// for (let i = 0; i < names.length; i++) {
+// 	console.log(names[i].length);
+// }
+
+// let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < number.length; i++) {
+// 	if (number[i] % 2 === 0) console.log(number[i]);
+// }
+
+// let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < number.length; i++) {
+// 	if (number[i] % 2 !== 0)
+// 		console.log(number[i]);
+// }
+
+// const number = [34, 6, 26, 48, 52, 88, 246];
+// const newnumber = [];
+// for (let i = 0; i < number.length; i++) {
+// 	newnumber.push(number[i] / 2);
+// }
+// console.log(newnumber);
+
+// let number = [100000000000000, 383883388383, 393832171, 555555555];
+// let js = 0;
+// for (let i = 0; i < number.length; i++) {
+// 	js += number[i];
+// }
+// console.log(js);
